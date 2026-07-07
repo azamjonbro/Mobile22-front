0eb365270a29981eb53246a96de62c4c8c91708b
azamjonbro <azamjondasturchi11@gmail.com>
2026-07-07 20:29:47 +0500

    feat: update API proxy target to use secure HTTPS endpoint

--- vite.config.js
+++ vite.config.js
@@ -98,7 +98,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://mobile.techinfo.uz',
         changeOrigin: true,
         secure: false
       }
